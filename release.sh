2ba32f44dfddec4ef9756ce46e78cab1379a4ad4
Einar Ingebrigtsen <einari@me.com>
2024-03-23 09:48:28 +0100

    Making release possible to do automatically based on existing version or manual override

--- release.sh
+++ release.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-VERSION_TO_RELASE=$(curl -sS  https://azuresearch-usnc.nuget.org/query\?q\=grpc.core | jq -r '.data[] | select(.id=="Grpc.Core")' | jq -r '.versions[-1:]' | jq -r '.[0].version')
-CURRENT_VERSION=$(curl -sS  https://azuresearch-usnc.nuget.org/query\?q\=Contrib.Grpc.Core.M1 | jq -r '.data[] | select(.id=="Contrib.Grpc.Core.M1")' | jq -r '.versions[-1:]' | jq -r '.[0].version')
+if [ -n "$1" ]; then
+    VERSION_TO_RELASE="$1"
+fi
 
-echo "Current version: $CURRENT_VERSION"
+if [ -z "$VERSION_TO_RELASE" ]; then
+    VERSION_TO_RELASE=$(curl -sS  https://azuresearch-usnc.nuget.org/query\?q\=grpc.core | jq -r '.data[] | select(.id=="Grpc.Core")' | jq -r '.versions[-1:]' | jq -r '.[0].version')
+fi
+
+CURRENT_VERSION=$(curl -sS  https://azuresearch-usnc.nuget.org/query\?q\=Contrib.Grpc.Core.M1 | jq -r '.data[] | select(.id=="Contrib.Grpc.Core.M1")' | jq -r '.versions[-1:]' | jq -r '.[0].version')
+echo "Current Grpc.Core version: $CURRENT_VERSION"
 echo "Version to release: $VERSION_TO_RELASE"
 
 if [ "$VERSION_TO_RELASE" == "$CURRENT_VERSION" ]; then
@@ -11,6 +17,7 @@ if [ "$VERSION_TO_RELASE" == "$CURRENT_VERSION" ]; then
 else
     echo "New version to release"
     sed -i -e "s/INSERTVERSION/${VERSION_TO_RELASE}/g" Grpc.Core.M1.nuspec
+    sed -i -e "s/CURRENTVERSION/${CURRENT_VERSION}/g" Grpc.Core.M1.nuspec
     rm *.nupkg
     nuget pack
     nuget push *.nupkg -source https://api.nuget.org/v3/index.json    
